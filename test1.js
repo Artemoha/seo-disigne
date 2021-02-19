93f79b35bbea3da5b8c527e6fafeaa4ea6172f5f
Artemoha <65351219+Artemoha@users.noreply.github.com>
2021-02-19 15:44:52 +0300

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,22 @@
+(function() {
+	'use strict';
+
+	BX.namespace('BX.Sale.BasketComponent');
+	ids: {
+			item: 'basket-item-',
+			quantity: 'basket-item-quantity-',
+			price: 'basket-item-price-',
+			sumPrice: 'basket-item-sum-price-',
+			sumPriceOld: 'basket-item-sum-price-old-',
+			sumPriceDiff: 'basket-item-sum-price-difference-',
+			itemHeightAligner: 'basket-item-height-aligner-',
+			total: 'basket-total-price',
+			basketRoot: 'basket-root',
+			itemListWrapper: 'basket-items-list-wrapper',
+			itemListContainer: 'basket-items-list-container',
+			itemList: 'basket-item-list',
+			itemListTable: 'basket-item-table',
+			itemListEmptyResult: 'basket-item-list-empty-result',
+			itemListOverlay: 'basket-items-list-overlay',
+			warning: 'basket-warning'
+		}
